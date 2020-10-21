2ad8dbea3bdd4830f323eb7bb5c95c0083cd3187
Jonatan Tran <jonathan.t79@hotmail.com>
2020-10-21 18:59:22 +0200

    Add project files.

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map.CreateMap();
+        }
+    }
+    class Map
+    {
+        public static void CreateMap()
+        {
+            //print horizontel
+            for (int i = 0; i < 99; i++)
+            {
+                Console.SetCursorPosition(i + 10, 0);
+                Console.Write("#");
+            }
+            for (int i = 0; i < 99; i++)
+            {
+                Console.SetCursorPosition(i + 10, 23);
+                Console.Write("#");
+            }
+            //Print vertical
+            for (int i = 0; i < 23; i++)
+            {
+                Console.WriteLine("#");
+            }
+        }
+    }
+}
